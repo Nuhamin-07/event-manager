6738bb21820067c450aae8de113cc0f468e72d14
Nuhamin <nuhamin.gulilat.7@gmail.com>
2026-09-01 16:06:20 -0400

    protecting routes with middleware

---
+++ proxy.ts
@@ -0,0 +1,6 @@
+import { NextRequest } from "next/server";
+
+export default async function proxy(req: NextRequest) {
+  const { auth } = await import("@/lib/auth/server");
+  return auth.middleware({ loginUrl: "/auth/signi-in" })(req);
+}
