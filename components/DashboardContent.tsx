e0a2163baf277aefbf55a717b237c378277f4e03
Nuhamin <nuhamin.gulilat.7@gmail.com>
2026-09-02 17:42:33 -0400

    dashboard component UI

--- components/DashboardContent.tsx
+++ components/DashboardContent.tsx
@@ -1,11 +1,17 @@
+import { Button } from "./ui/button";
+import Link from "next/link";
+
 export async function DashboardContent({ userId }: { userId: string }) {
   return (
-    <div>
-      <div>
+    <div className="flex flex-1 flex-col gap-6">
+      <div className="flex flex-wrap items-center justify-between gap-3">
         <div>
           <h1>Your Events</h1>
           <p>Track atendees response and manage invite links.</p>
         </div>
+        <Button asChild>
+          <Link href="/event/new">Create Event</Link>
+        </Button>
       </div>
     </div>
   );
