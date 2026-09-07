7316e02203abb7ef764f309212e06b359c50eb93
Nuhamin <nuhamin.gulilat.7@gmail.com>
2026-09-07 17:14:01 -0400

    RSVP form added

--- components/InviteRsvpContent.tsx
+++ components/InviteRsvpContent.tsx
@@ -5,6 +5,8 @@ import { Card, CardContent, CardHeader, CardTitle } from "./ui/card";
 import { Badge } from "./ui/badge";
 import { type RsvpStatus as PrismaRsvpStatus } from "@/app/generated/prisma/enums";
 import { notFound } from "next/navigation";
+import { Label } from "./ui/label";
+import { Input } from "./ui/input";
 
 export async function InviteRsvpContent({
   token,
@@ -58,7 +60,57 @@ export async function InviteRsvpContent({
             <p className="text-sm text-muted-foreground">{event.description}</p>
           ) : null}
         </CardHeader>
-        <CardContent></CardContent>
+        <CardContent>
+          {submitted ? (
+            <p className="mb-4 rounded-md border border-(--accent)">
+              Thanks. Your RSVP has been recorded.
+            </p>
+          ) : (
+            <form
+              //   action={submitInviteAction}
+              className="space-y-6"
+            >
+              <div className="space-y-2">
+                <Label htmlFor="name">Name</Label>
+                <Input
+                  id="name"
+                  name="name"
+                  placeholder="Enter your name"
+                  required
+                />
+              </div>
+
+              <div className="space-y-2">
+                <Label htmlFor="email">Email</Label>
+                <Input
+                  id="email"
+                  name="email"
+                  type="email"
+                  placeholder="Enter your email"
+                  required
+                />
+              </div>
+
+              <div className="space-y-2">
+                <Label htmlFor="status">Attendance</Label>
+                <select
+                  id="status"
+                  name="status"
+                  className="flex h-10 w-full rounded-md border border-(--border) bg-(--surface) px-3 py-1 text-sm shadow-xs"
+                  required
+                >
+                  <option value="going">Going</option>
+                  <option value="maybe">Maybe</option>
+                  <option value="not_going">Not Going</option>
+                </select>
+              </div>
+
+              <div className="flex items-center gap-3">
+                <Button type="submit">Submit RSVP</Button>
+              </div>
+            </form>
+          )}
+        </CardContent>
       </Card>
     </div>
   );
