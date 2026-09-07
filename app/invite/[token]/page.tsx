88e208f355a1ffb0978870e0960a3b02c05c4022
Nuhamin <nuhamin.gulilat.7@gmail.com>
2026-09-07 16:13:32 -0400

    Invite page created

---
+++ app/invite/[token]/page.tsx
@@ -0,0 +1,17 @@
+import { EventDetailContent } from "@/components/EventDetailContent";
+import { getSession } from "@/lib/auth/server";
+
+export default async function InvitePage({
+  params,
+  searchParams,
+}: {
+  params: Promise<{ token: string }>;
+  searchParams: Promise<{ submitted?: string }>;
+}) {
+  const { token } = await params;
+  const query = await searchParams;
+  const session = await getSession();
+  return (
+    <InviteRsvpContent token={token} submitted={(query.submitted = "1")} />
+  );
+}
