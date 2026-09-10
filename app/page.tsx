077cf1dee44328fedcb5a0b3c56e00da3b516fca
Nuhamin <nuhamin.gulilat.7@gmail.com>
2026-09-10 17:23:40 -0400

    home page updated

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,13 @@ export default function Home() {
   return (
     <div className="flex flex-1 flex-col gap-8">
       <section className="space-y-4">
-        <Badge variant="secondary" className="w-fit"></Badge>
+        <h1 className="text-4xl font-semibold tracking-tight">
+          Plan events and track RSVP fast.
+        </h1>
+        <p className="max-w-2xl text-(--muted-foreground)">
+          Create events, share a unique invite link and watch atendees status
+          updated in real-time with Going, Maybe and Not Going counts.
+        </p>
       </section>
     </div>
   );
