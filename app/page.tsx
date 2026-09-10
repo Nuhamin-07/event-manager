447cb54b0e72ba0af2a46bb78447c2554b32bbd5
Nuhamin <nuhamin.gulilat.7@gmail.com>
2026-09-10 17:36:50 -0400

    info cards are added

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,12 @@
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 import Link from "next/link";
 
 export default function Home() {
@@ -24,6 +31,36 @@ export default function Home() {
             <Link href="/dshboard">Dashboard</Link>
           </Button>
         </div>
+      </section>
+
+      <section className="grid gap-4 md:grid-cols-3">
+        <Card>
+          <CardHeader>
+            <CardTitle>Create Events</CardTitle>
+            <CardDescription>
+              Set title, date and details in second.
+            </CardDescription>
+          </CardHeader>
+        </Card>
+        <Card>
+          <CardHeader>
+            <CardTitle>Shard invite links</CardTitle>
+            <CardDescription>
+              Generate a unique event token for each events.
+            </CardDescription>
+          </CardHeader>
+        </Card>
+        <Card>
+          <CardHeader>
+            <CardTitle>Track attendance</CardTitle>
+            <CardDescription>
+              Set attendeelist and response totals at a glance.
+            </CardDescription>
+          </CardHeader>
+          <CardContent className="text-sm text-(--muted-foreground)">
+            Going, Maybe and Not Going statuses are always up-to-date.
+          </CardContent>
+        </Card>
       </section>
     </div>
   );
