a41b4e8a690e795927b812cc267726118be8fdc8
Nuhamin <nuhamin.gulilat.7@gmail.com>
2026-09-10 17:18:10 -0400

    home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,11 @@
+import { Badge } from "@/components/ui/badge";
+
 export default function Home() {
-  return <div>Home page</div>;
+  return (
+    <div className="flex flex-1 flex-col gap-8">
+      <section className="space-y-4">
+        <Badge variant="secondary" className="w-fit"></Badge>
+      </section>
+    </div>
+  );
 }
